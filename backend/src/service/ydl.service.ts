4a92bafbcbcc2f920c79e76f336738bdabefffc4
rajaksatyam <satyamrajak@protonmail.com>
2026-06-15 09:35:48 +0000

    Fix Youtube headers.

--- backend/src/service/ydl.service.ts
+++ backend/src/service/ydl.service.ts
@@ -10,15 +10,39 @@ export const download = async (URL: string) => {
 
 
 
+  // const options: any = {
+  //   preferFreeFormats: true,
+  //   dumpSingleJson: true,   
+  //   noWarnings: true,
+  //   cookies: URL.includes("instagram")=== true ?'/app/cookies.txt': null,
+  //   addHeader: ['referer:instagram.com',
+  //     'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'] as any
+  // };
+
+  const isInstagram = URL.includes("instagram.com");
+  const isYouTube = URL.includes("youtube.com") || URL.includes("youtu.be");
+
+
   const options: any = {
     preferFreeFormats: true,
     dumpSingleJson: true,   
     noWarnings: true,
-    cookies: URL.includes("instagram")=== true ?'/app/cookies.txt': null,
-    addHeader: ['referer:instagram.com',
-      'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'] as any
   };
 
+  if (isInstagram) {
+    options.cookies = '/app/cookies.txt';
+    options.addHeader = [
+      'referer:instagram.com',
+      'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    ];
+  } else if (isYouTube) {
+    options.format = 'ext=mp4/best'; 
+    options.addHeader = [
+      'referer:youtube.com',
+      'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    ];
+    // options.cookies = '/app/youtube_cookies.txt'; 
+  }
 
   const info = await YTD(URL, options);
   const cdnURL = info.url ?? info.formats?.at(-1)?.url
